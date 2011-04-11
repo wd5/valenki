c22385b2274a9e745d4fca3e7ed7caaccde5dea8
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-04-11 15:01:22 +0400

    Bug fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('',
                       url(r'^cats/(?P<category_slug>[-\w]+)/$', 'catalog.views.show_category', name="catalog-page"),
                       url(r'^products/(?P<product_slug>[-\w]+)/$', 'catalog.views.show_product', name="product-page"),
                       url(r'^about$', 'catalog.views.about', name="about-page"),
-                      url(r'^blog$', 'catalog.views.blog', name="blog-page"),
-                      url(r'^delivery$', 'catalog.views.delivery', name="delivery-page"),)
+                      url(r'^delivery$', 'catalog.views.delivery', name="delivery-page"),
+                      url(r'^size$', 'catalog.views.size', name="size-page"),
+                      url(r'^buy$', 'catalog.views.buy', name="buy-page"),
+                      url(r'^contract$', 'catalog.views.contract', name="contract-page"),)
 
